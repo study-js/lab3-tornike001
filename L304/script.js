21245f9a568c14a745e6ac8847066793e642df20
tornike001 <33287858+tornike001@users.noreply.github.com>
2017-11-19 14:02:55 +0400

    L304

---
+++ L304/script.js
@@ -0,0 +1,13 @@
+var sheyvana = prompt("რამდენი სტუდენტის შეყვანა გსურთ?");
+
+var students = [];
+
+for (var i = 0; i < sheyvana ; ++i) {
+    
+    var saxelebi = prompt("სახელები: ");  
+
+    students.push(saxelebi);
+    
+}
+
+document.write(students);
